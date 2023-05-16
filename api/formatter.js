7099b2db5b01f676b6673e7710f00e98fac994e7
Irwing Tello <28800239+irwingtello@users.noreply.github.com>
2023-05-16 11:22:11 -0500

    Changed the body params for the only one and data response standardizedData

--- api/formatter.js
+++ api/formatter.js
@@ -5,20 +5,19 @@ module.exports = async (req, res) => {
   switch (req.method) {
     case "POST":
       try {
-        const ens = req.body.ens;
-        const address=req.body.address;
-        const ensOption=req.body.ensOption;
-        const addressOption=req.body.addressOption;
+        const wallet = req.body.wallet;
+        const containsENSDomain = /^[a-zA-Z0-9-]+\.eth$/.test(wallet);
         let data = JSON.stringify({
           "query": "query NFTsByENS($ensName: String!, $address: String!,$ensOption:Boolean!,$addressOption:Boolean!){ethereum{walletByENS(ensName: $ensName) @include(if: $ensOption){walletCollections{totalCount edges{node{collection{name address image{url}}nftsCount}}}}walletByAddress(address: $address) @include(if: $addressOption){walletCollections{totalCount edges{node{collection{name address image{url}}nftsCount}}}}}}",
           "variables": {
-            "ensName": ens,
-            "address": address,
-            "ensOption": ensOption,
-            "addressOption": addressOption
+            "ensName": wallet,
+            "address": wallet,
+            ensName: containsENSDomain ? wallet : "",
+            address: containsENSDomain ? "" : wallet,
+            ensOption: containsENSDomain,
+            addressOption: !containsENSDomain
           }
         });
-
         let config = {
             headers: { 
               'x-api-key': process.env.APIKEY, 
@@ -32,7 +31,26 @@ module.exports = async (req, res) => {
           config
         );
 
-        res.status(200).json(response.data);
+        // Standardize the response data
+        const standardizedData = {
+          data: {
+            ethereum: {
+              walletCollections: {
+                totalCount: 0,
+                edges: []
+              }
+            }
+          }
+        };
+
+
+        if (response.data.data?.ethereum?.walletByENS?.walletCollections) {
+          standardizedData.data.ethereum.walletCollections = response.data.data.ethereum.walletByENS.walletCollections;
+        } else if (response.data.data?.ethereum?.walletByAddress?.walletCollections) {
+          standardizedData.data.ethereum.walletCollections = response.data.data.ethereum.walletByAddress.walletCollections;
+        }
+
+        res.status(200).json(standardizedData );
 
       } catch (error) {
         return res.status(500).json({ error: error.message });
@@ -43,4 +61,5 @@ module.exports = async (req, res) => {
     default:
       return res.status(405).json({ message: "Method Not Allowed" });
   }
+  
 };
